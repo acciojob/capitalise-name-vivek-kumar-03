8799e28176f241bc123f092362cf3b6602f51827
VIVEK KUMAR <vk0730267@gmail.com>
2025-06-21 21:01:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const input = document.getElementById('fname');
+
+input.addEventListener('blur', () => {
+  input.value = input.value.toUpperCase();
+});
+
